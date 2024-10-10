2bc666de0efcafedcba7f7d41d838ed814c2861e
JudeAnthone <151014535+JudeAnthone@users.noreply.github.com>
2024-10-10 08:35:33 +0800

    Updated posting_act.cpp

--- posting_act.cpp
+++ posting_act.cpp
@@ -1,152 +1,151 @@
-#include <iostream>
+ #include <iostream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
-  const int maxPost = 10;
-  const int maxComment = 10;
-  const int maxSubcomment = 10;
+const int maxPost = 10;
+const int maxComment = 10;
+const int maxSubcomment = 10;
 
-    string post[maxPost];                                   // storing post in array
-    string comments[maxPost][maxComment];                   // storing comment in 2d array
-    string subcomments[maxPost][maxComment][maxSubcomment]; // storing subcomment in 3d array
+string post[maxPost];                                   // storing post in array
+string comments[maxPost][maxComment];                   // storing comment in 2d array
+string subcomments[maxPost][maxComment][maxSubcomment]; // storing subcomment in 3d array
 
-      int postCount = 0;                               // tracking number of post
-      int commentCount[maxPost] = {0};                 // tracking comments per post
-      int subComment_count[maxPost][maxComment] = {0}; // tracking sub comments per comment
+int postCount = 0;                               // tracking number of post
+int commentCount[maxPost] = {0};                 // tracking comments per post
+int subComment_count[maxPost][maxComment] = {0}; // tracking sub comments per comment
 
-        // function declarations
-        void myMenu();
-        void createPost();
-        void addComment();
-        void addSubcomment();
-        void viewSpecificPost();
-        void viewNewsfeed();
-        void displayPost(int postID); // function for viewing post with all of its comment and subcomment
+// function declarations
+void myMenu();
+void createPost();
+void addComment();
+void addSubcomment();
+void viewSpecificPost();
+void viewNewsfeed();
+void displayPost(int postID); // function for viewing post with all of its comment and subcomment
+void clearInput();
 
-    int main()
+int main()
+{
+
+  int choice;
+
+  do
+  {
+    myMenu();
+
+    while (!(cin >> choice))
     {
-
-      int choice;
-
-      do
-      {
-        myMenu();
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-          createPost();
-          break;
-
-        case 2:
-          addComment();
-          break;
-
-        case 3:
-          addSubcomment();
-          break;
-
-        case 4:
-          viewSpecificPost();
-          break;
-
-        case 5:
-          viewNewsfeed();
-          break;
-
-        case 6:
-          cout << "EXITING PROGRAM \n";
-          break;
-        default:
-          cout << "ERROR: Invalid Choice! \n ";
-        }
-
-      } while (choice != 6);
-
-      return 0;
+      cout << "ERROR: Invalid Input. Please enter numbers 1-6 only. \n";
+      clearInput();
     }
 
+    switch (choice)
+    {
+    case 1:
+      createPost();
+      break;
 
+    case 2:
+      addComment();
+      break;
 
-//MENU PANEL
-  void myMenu()
-  {
+    case 3:
+      addSubcomment();
+      break;
 
-    cout << "\n";
-    cout << "\n";
-    cout << "EPBI DAT COM\n";
-    cout << "\n";
-    cout << "\n";
+    case 4:
+      viewSpecificPost();
+      break;
 
-    cout << "1. What's on your Mind? \n";
-    cout << "2. Comment \n";
-    cout << "3. Reply to a Comment \n";
-    cout << "4. View Post \n";
-    cout << "5. View All Post \n";
-    cout << "6. EXIT \n";
+    case 5:
+      viewNewsfeed();
+      break;
 
-    cout << "\n";
-    cout << "Enter your choice:";
-  }
+    case 6:
+      cout << "EXITING PROGRAM \n";
+      break;
+    default:
+      cout << "ERROR: Invalid Choice! \n ";
+    }
+
+  } while (choice != 6);
+
+  return 0;
+}
+
+// MENU PANEL
+void myMenu()
+{
+
+  cout << "\n===========================\n";
+  cout << "       EPBI DAT COM        \n";
+  cout << "===========================\n";
+  cout << "1. What's on your Mind?\n";
+  cout << "2. Comment\n";
+  cout << "3. Reply to a Comment\n";
+  cout << "4. View Post\n";
+  cout << "5. View All Posts\n";
+  cout << "6. EXIT\n";
+  cout << "===========================\n";
+  cout << "Enter your choice: ";
+}
 
 // function for creating a post
-    void createPost()
-    {
+void createPost()
+{
 
-      if (postCount >= maxPost)
-      {
-        cout << "ERROR: Limit post reached! \n";
-        return;
-      }
+  if (postCount >= maxPost)
+  {
+    cout << "ERROR: Limit post reached! \n";
+    return;
+  }
 
-      cout << "Whats on Your Mind?: \n";
-      cin.ignore();
-      getline(cin, post[postCount]);
-      cout << "Post Added! your Post ID is: " << postCount << '\n'; // the ID is per post count
-      postCount++;
-    }
+  cout << "Whats on Your Mind?: \n";
+  cin.ignore();
+  getline(cin, post[postCount]);
+  cout << "Post Added! your Post ID is: " << postCount << '\n'; // the ID is per post count
+  postCount++;
+}
 
-    // adding comments
-    void addComment()
-    {
-      int postID;
+// adding comments
+void addComment()
+{
+  int postID;
 
-        if (postCount == 0)
-        {
-          cout << "ERROR: Create a post first! \n";
-          return;
-        }
+  if (postCount == 0)
+  {
+    cout << "ERROR: Create a post first! \n";
+    return;
+  }
 
-      viewNewsfeed();
-      cout << "ENTER POST ID: ";
-      cin >> postID;
+  viewNewsfeed();
 
-          if (postID >= postCount || postID < 0)
-          {
-            cout << "ERROR: Invalid Post ID. Make sure that you entered a VALID ID! \n";
-            return;
-          }
+  cout << "ENTER POST ID: "; // cin >>
 
-            if (commentCount[postID] >= maxComment)
-            {
-              cout << "ERROR: Can't add comments. LIMIT REACHED! \n";
-              return;
-            }
+  while (!(cin >> postID) || postID < 0 || postID >= postCount)
+  {
+    cout << "ERROR: Invalid Post ID! Please enter a valid Post ID: ";
+    clearInput();
+  }
 
-      cout << "Enter your comment: ";
-      cin.ignore();
-      getline(cin, comments[postID][commentCount[postID]]);
-      cout << "Comment added Succesfully! ";
+  if (commentCount[postID] >= maxComment)
+  {
+    cout << "ERROR: Can't add comments. LIMIT REACHED! \n";
+    return;
+  }
 
-      commentCount[postID]++;
+  cout << "Enter your comment: ";
+  cin.ignore();
+  getline(cin, comments[postID][commentCount[postID]]);
+  cout << "Comment added Succesfully! ";
 
+  commentCount[postID]++;
 
-    // Display the updated post with the new comment that the user added
-      displayPost(postID);
-    }
-
+  // Display the updated post with the new comment that the user added
+  displayPost(postID);
+}
 
 // adding subComments
 void addSubcomment()
@@ -162,16 +161,14 @@ void addSubcomment()
   viewNewsfeed();
 
   cout << "Enter POST ID to add a reply.\n";
-  cin >> postID;
 
-//check if the ID inputted by the user is existing
-  if (postID >= postCount || postID < 0)
+  while (!(cin >> postID) || postID < 0 || postID >= postCount)
   {
-    cout << " ERROR: Invalid Post ID. Make sure that you entered a VALID ID.\n";
-    return;
+    cout << "ERROR: Invalid Post ID! Please enter a valid Post ID: ";
+    clearInput();
   }
 
-  //check if the post has a comment, if none, message will display
+  // check if the post has a comment, if none, message will display
   if (commentCount[postID] == 0)
   {
     cout << "No comments availble for this post. Add a comment first\n";
@@ -180,12 +177,13 @@ void addSubcomment()
 
   displayPost(postID);
   cout << "Enter COMMENT ID to add a reply.\n";
-  cin >> commentID;
 
-  if (commentID >= commentCount[postID] || commentID < 0)
+  // Ensure valid comment ID is entered
+  cout << "Enter COMMENT ID to add a reply.\n";
+  while (!(cin >> commentID) || commentID < 0 || commentID >= commentCount[postID])
   {
-    cout << "ERROR: Invalid post ID. \n";
-    return;
+    cout << "ERROR: Invalid Comment ID! Please enter a valid Comment ID: ";
+    clearInput();
   }
 
   if (subComment_count[postID][commentID] >= maxSubcomment)
@@ -197,14 +195,13 @@ void addSubcomment()
   cout << "Enter your reply: ";
   cin.ignore();
   getline(cin, subcomments[postID][commentID][subComment_count[postID][commentID]]);
-  cout << "Your reply is added sucesfully!\n";
+  cout << "Your reply is added succesfully!\n";
 
   subComment_count[postID][commentID]++;
 
-//display the updated post with the new subcomment-comment
+  // display the updated post with the new subcomment-comment
   displayPost(postID);
 }
-
 
 // view a specific post based on ID that the user INPUTS
 void viewSpecificPost()
@@ -220,7 +217,7 @@ void viewSpecificPost()
   cout << "Enter the Post ID: ";
   cin >> postID;
 
-// displays if the ID is not existing
+  // displays if the ID is not existing
   if (postID >= postCount || postID < 0)
   {
     cout << "ERROR: INVALID POST ID! \n";
@@ -229,7 +226,6 @@ void viewSpecificPost()
 
   displayPost(postID);
 }
-
 
 // view all the posts (10 post max)
 void viewNewsfeed()
@@ -240,13 +236,14 @@ void viewNewsfeed()
     return;
   }
 
+  cout << "\n=========== NEWSFEED ===========\n";
+
   for (int i = 0; i < postCount; i++)
   {
     displayPost(i);
   }
+  cout << "=================================\n";
 }
-
-
 
 // displaying a post with its components (comments and subcomments)
 void displayPost(int postID)
@@ -263,7 +260,9 @@ void displayPost(int postID)
     return;
   }
 
-  cout << "\n Post ID: " << postID << "; " << post[postID] << "\n";
+  cout << "\n Post ID: " << postID << "\n";
+  cout << post[postID] << "\n";
+  cout << "=================================\n";
 
   if (commentCount[postID] == 0)
   {
@@ -273,13 +272,13 @@ void displayPost(int postID)
   {
     for (int j = 0; j < commentCount[postID]; j++)
     {
-        // for comments
-      cout << " Comment " << j << ": " << comments[postID][j] << '\n';
+      // for comments
+      cout << "\n    Comment " << j << ": " << comments[postID][j] << '\n';
 
-        // reply to a comment
+      // reply to a comment
       if (subComment_count[postID][j] == 0)
       {
-        cout << "No Replies.\n";
+        cout << "         No Replies.\n";
       }
 
       else
@@ -287,9 +286,16 @@ void displayPost(int postID)
         // reply to a comment or a subcomment
         for (int k = 0; k < subComment_count[postID][j]; k++)
         {
-          cout << "   Replies " << k << ": " << subcomments[postID][j][k] << "\n";
+          cout << "             Replies " << k << ": " << subcomments[postID][j][k] << "\n";
         }
       }
     }
   }
+  cout << "=================================\n";
+}
+
+void clearInput()
+{
+  cin.clear();
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
